f2bd043a67302e4841175f7957bb14519164c3ff
Alex <alexdeassis7@gmail.com>
2021-03-06 13:03:14 -0300

    Clase 1 - demo primer proyecto - variables-printf

---
+++ Clase1HolaMundo/main.c
@@ -0,0 +1,39 @@
+//incluimos librerias
+#include <stdio.h>
+#include <stdlib.h>
+
+//punto de inicio de ejecucion del programa
+//Funcion principal
+int main()
+{
+        //mostramos un mensaje por pantalla
+    printf("Hello world!\n");
+
+    //Tipos de Datos basicos en lenguaje C
+    //definicion de variables:
+
+    //tipoDedato nombreDeLaVarible ;
+    char sexo = 'M'; //defino e inicializo una variable
+    int edad ;//defino
+    edad = 28;//inicializo
+    float peso = 70;
+    double medicamento = 485288.564654;
+    //void sin valores
+
+    int numero1 = 24 , numero2 = 45 ;
+
+    int resultado = numero1 + numero2;
+
+    printf("soy un mensaje por pantalla \n");
+
+    printf("el valor de numero1 es : %d \n" , numero1);
+
+    printf("el valor de numero2 es : %d \n" , numero2);
+
+    printf("el valor de resultado es : %d \n" , resultado);
+
+    printf("el valor del peso es : %f \n" , peso);
+
+    return 0;
+
+}
